5dcfc85bf91e5e503655c306efca4094ff067e13
Jude Tulel <judeyego202@gmail.com>
2024-06-25 13:36:30 +0300

    VehicleComponent

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Home from './Pages/Home'
+import AddVehicle from './components/AddVehicle'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
-    <Home/>
+    <AddVehicle />
     </div>
   )
 }

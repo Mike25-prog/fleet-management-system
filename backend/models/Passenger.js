781625332ea68c77f22cd369b18ad368c016f3d7
Jude Tulel <judeyego202@gmail.com>
2024-07-15 11:40:33 +0300

    hapama pale

--- backend/models/Passenger.js
+++ backend/models/Passenger.js
@@ -5,10 +5,18 @@ const Passenger = {
     create: async (data, callback) => {
         try {
             console.log('Data received for passenger creation:', data);
+            
+            // Hash the password
             const hashedPassword = await bcrypt.hash(data.password, 10);
+            
+            // SQL query to insert a new passenger
             const query = 'INSERT INTO passengers SET ?';
+            
+            // Create a passenger object with hashed password
             const passengerData = { ...data, password: hashedPassword };
             console.log('Query and passengerData:', query, passengerData);
+            
+            // Execute the query
             connection.query(query, passengerData, (err, results) => {
                 if (err) {
                     console.error('Error in query:', err);
@@ -21,8 +29,12 @@ const Passenger = {
             callback(error);
         }
     },
+    
     findByEmail: (email, callback) => {
+        // SQL query to find a passenger by email
         const query = 'SELECT * FROM passengers WHERE email = ?';
+        
+        // Execute the query
         connection.query(query, [email], (err, results) => {
             if (err) {
                 console.error('Error in query:', err);

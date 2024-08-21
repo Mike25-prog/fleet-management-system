0ee0c9cbe1f76ccee2c13e9a2d496aeda7de3046
Jude Tulel <judeyego202@gmail.com>
2024-08-21 21:00:59 +0300

    Man huwezi change kitu inafanya

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,29 +1,36 @@
-const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = require('./db');
+const bcrypt = require('bcrypt');
+const connection = require('../config/db');
 
-const User = sequelize.define('User', {
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  first_name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  last_name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-});
+const User = {
+    create: async (data, callback) => {
+        try {
+            console.log('Data received for user creation:', data);
+            const hashedPassword = await bcrypt.hash(data.password, 10);
+            const query = 'INSERT INTO users SET ?';
+            const userData = { ...data, password: hashedPassword };
+            console.log('Query and userData:', query, userData);
+            connection.query(query, userData, (err, results) => {
+                if (err) {
+                    console.error('Error in query:', err);
+                    return callback(err);
+                }
+                callback(null, results);
+            });
+        } catch (error) {
+            console.error('Error in bcrypt:', error);
+            callback(error);
+        }
+    },
+    findByUsername: (username, callback) => {
+        const query = 'SELECT * FROM users WHERE username = ?';
+        connection.query(query, [username], (err, results) => {
+            if (err) {
+                console.error('Error in query:', err);
+                return callback(err);
+            }
+            callback(null, results[0]);
+        });
+    }
+};
 
 module.exports = User;
